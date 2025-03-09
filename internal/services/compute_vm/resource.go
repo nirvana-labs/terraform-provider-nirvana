5b7ac17bd5cca778ea16807ce602e24d4217e34c
Akram Hussein <akram@nirvanalabs.io>
2025-03-09 16:55:45 -0300

    feat(compute/vm): add wait for operation in Create

--- internal/services/compute_vm/resource.go
+++ internal/services/compute_vm/resource.go
@@ -70,11 +70,24 @@ func (r *ComputeVMResource) Create(ctx context.Context, req resource.CreateReque
 		resp.Diagnostics.AddError("failed to serialize http request", err.Error())
 		return
 	}
-	res := new(http.Response)
-	_, err = r.client.Compute.VMs.New(
+	operation, err := r.client.Compute.VMs.New(
 		ctx,
 		compute.VMNewParams{},
 		option.WithRequestBody("application/json", dataBytes),
+		option.WithMiddleware(logging.Middleware(ctx)),
+	)
+	if err != nil {
+		resp.Diagnostics.AddError("failed to make http request", err.Error())
+		return
+	}
+	if errWaitForOperation := lib.Wait(ctx, r.client, operation.ID); errWaitForOperation != nil {
+		resp.Diagnostics.AddError("failed to wait for operation", errWaitForOperation.Error())
+		return
+	}
+	res := new(http.Response)
+	_, err = r.client.Compute.VMs.Get(
+		ctx,
+		operation.ResourceID,
 		option.WithResponseBodyInto(&res),
 		option.WithMiddleware(logging.Middleware(ctx)),
 	)
